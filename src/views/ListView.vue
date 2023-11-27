b33e13c651fd1f7fe02ca0af2a75522a8a10cb65
lensigonzales <lena.si@outlook.com>
2023-11-27 13:56:49 +0100

    feat: add list view functionality

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,6 +1,19 @@
 <template>
-  <h1>Hier ist die list view</h1>
-  <div>{{ store.getListByStatus($route.params.status) }}</div>
+  <header>
+    <h1 v-if="$route.params.status == 'visited'">Visited</h1>
+    <h1 v-if="$route.params.status == 'wish'">Wishlist</h1>
+    <h1 v-if="$route.params.status == 'upcoming'">Upcoming</h1>
+  </header>
+  <ul>
+    <li v-for="concert of currentList" :key="concert.id">
+      <router-link :to="{ name: 'detail', params: { id: concert.id } }">
+        {{ concert.name }} {{ concert.dates.start.localDate }} -
+        {{ concert.dates.start.localTime }} ,
+        {{ concert._embedded.venues[0].city.name }} -
+        {{ concert._embedded.venues[0].country.name }}</router-link
+      >
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -12,6 +25,11 @@ export default {
     return {
       store: useStore(),
     };
+  },
+  computed: {
+    currentList() {
+      return this.store.getListByStatus(this.$route.params.status);
+    },
   },
 };
 </script>
