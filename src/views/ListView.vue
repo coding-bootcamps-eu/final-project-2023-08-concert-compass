b389418a57e6a0da4c6459025d3b1725cc7a906b
lensigonzales <lena.si@outlook.com>
2023-11-28 23:23:38 +0100

    feat(listview): add styling

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,19 +1,57 @@
 <template>
-  <header>
-    <h1 v-if="$route.params.status == 'visited'">Visited</h1>
-    <h1 v-if="$route.params.status == 'wish'">Wishlist</h1>
-    <h1 v-if="$route.params.status == 'upcoming'">Upcoming</h1>
-  </header>
-  <ul>
-    <li v-for="concert of currentList" :key="concert.id">
-      <router-link :to="{ name: 'detail', params: { id: concert.id } }">
-        {{ concert.name }} {{ concert.dates.start.localDate }} -
-        {{ concert.dates.start.localTime }} ,
-        {{ concert._embedded.venues[0].city.name }} -
-        {{ concert._embedded.venues[0].country.name }}</router-link
+  <header
+    class="border-transparent rounded-b-lg h-1/4 flex items-center justify-center relative"
+  >
+    <router-link class="fixed top-6 left-6" :to="{ name: 'home' }">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
       >
-    </li>
-  </ul>
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18"
+        />
+      </svg>
+    </router-link>
+    <h1 class="text-3xl font-bold" v-if="$route.params.status == 'visited'">
+      Visited
+    </h1>
+    <h1 class="text-3xl font-bold" v-if="$route.params.status == 'wish'">
+      Wishlist
+    </h1>
+    <h1 class="text-3xl font-bold" v-if="$route.params.status == 'upcoming'">
+      Upcoming
+    </h1>
+  </header>
+  <main class="mx-6">
+    <ul class="mt-18">
+      <li
+        class="border-transparent rounded-lg shadow-xl mt-4 text-white-color bg-cover max-w-[500px] min-h-min"
+        v-for="concert of currentList"
+        :key="concert.id"
+        :style="{ backgroundImage: `url(${concert.images?.[0].url})` }"
+      >
+        <div
+          class="h-fit w-full bg-gray-overlay rounded-lg hover:bg-transparent"
+        >
+          <router-link :to="{ name: 'detail', params: { id: concert.id } }">
+            <div class="p-4">
+              <h6 class="text-lg font-semibold">{{ concert.name }}</h6>
+              <p class="text-sm">
+                {{ concert.dates.start.localDate }} &nbsp;
+                {{ concert._embedded.venues[0].city.name }}
+              </p>
+            </div>
+          </router-link>
+        </div>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,18 @@ export default {
   theme: {
     colors: {
       "dark-color": "#2E223C",
+      "black-color": "#000000",
       "violett-color": "#7A4DF0",
       "orange-color": "#FFBB00",
       "light-blue": "#00DCFF",
       "white-color": "#FFFFFF",
       "gray-ash": "#808080",
+      "gray-overlay": "rgba(0, 0, 0, 0.5)",
     },
-
+    backgroundImage: {
+      "gradient-black":
+        "linear-gradient(to top, rgba(0,0,0,1), rgba(0,0,0,.7), rgba(0,0,0,.4), rgba(0,0,0,0))",
+    },
     fontFamily: {
       sans: ["Raleway", "sans-serif"],
     },
