319ea3a9de66cf9bee828febb9d63d7f6580b102
lensigonzales <lena.si@outlook.com>
2023-11-28 09:46:54 +0100

    feat(detail): add styling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
     <router-view />
-    <router-link :to="{ name: 'home' }"
-      ><img src="./assets/home.svg" alt="Home" width="20" height="20" />
-    </router-link>
-    <router-link :to="{ name: 'search' }"
-      ><img src="./assets/search.svg" alt="Search" width="20" height="20" />
-    </router-link>
-    <span
-      ><img src="./assets/discover.svg" alt="Discover" width="20" height="20" />
-    </span>
-    <span
-      ><img src="./assets/user.svg" alt="Profile" width="20" height="20"
-    /></span>
+    <nav class="grid grid-cols-4 place-content-evenly justify-items-center">
+      <router-link :to="{ name: 'home' }"
+        ><img src="./assets/home.svg" alt="Home" width="20" height="20" />
+      </router-link>
+      <router-link :to="{ name: 'search' }"
+        ><img src="./assets/search.svg" alt="Search" width="20" height="20" />
+      </router-link>
+      <span
+        ><img
+          src="./assets/discover.svg"
+          alt="Discover"
+          width="20"
+          height="20"
+        />
+      </span>
+      <span
+        ><img src="./assets/user.svg" alt="Profile" width="20" height="20"
+      /></span>
+    </nav>
   </div>
 </template>
 
