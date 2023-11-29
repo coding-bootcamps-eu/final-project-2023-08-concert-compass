951157f2b6a1a833b6a192b7916e99496fee9cd9
Marco Rosenbaum <marco.rosenbaum95@gmail.com>
2023-11-29 11:40:51 +0100

    fix note bug

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -129,10 +129,10 @@
 
     <section class="border-transparent rounden-t-lg px-6 py-6">
       <!-- Add to List -->
-      <form v-if="hasStatus" @submit.prevent="notes = currentConcert.notes">
+      <form v-if="hasStatus" @submit.prevent="currentConcert.notes = notes">
         <label for="notes">Notes</label>
         <textarea
-          v-model="currentConcert.notes"
+          v-model="notes"
           class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           type="text"
           name="concert-notes"
